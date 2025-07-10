5ffd587dbb1006b3fd6225a7b2ad1cd6bd71b419
bdcf <53155385+bdcf@users.noreply.github.com>
2025-07-09 23:26:44 -0400

    Create osDemo.js
    Created a file with demonstrations of functions in the os module.

---
+++ osDemo.js
@@ -0,0 +1,9 @@
+import os from 'os';
+
+console.log(os.userInfo);// logs information about the current user
+
+console.log(os.totalmem);// logs the total system memory in bytes
+
+console.log(os.freemem);// logs the amount of free system memory in bytes
+
+console.log(os.cpus);// gives an object for every cpu on system
